43ae5dc104c7c3e96c8bc97d7d3ca742176cb750
tamberg <tamberg@yaler.net>
2020-08-03 21:52:46 +0200

    Added example.

---
+++ data-transport/CircuitPython/lorawan/lorawan.py
@@ -0,0 +1,43 @@
+# Based on https://github.com/adafruit/Adafruit_CircuitPython_TinyLoRa licensed under LGPL (!)
+
+import time
+import busio
+import digitalio
+import board
+from adafruit_tinylora.adafruit_tinylora import TTN, TinyLoRa
+
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+# FeatherWing RFM95 with nRF52840
+cs = digitalio.DigitalInOut(board.D5)
+irq = digitalio.DigitalInOut(board.D10)
+rst = digitalio.DigitalInOut(board.D6)
+
+# TODO: App > Device > Settings (ABP) from
+# TTN https://console.thethingsnetwork.org/
+ttn_dev_address_msb = bytearray([0x00, 0x00, 0x00, 0x00])
+
+ttn_net_session_key_msb = bytearray(
+    [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+ttn_app_session_key_msb = bytearray(
+    [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+ttn_region = "EU" # 868 MHz
+ttn_config = TTN(
+    ttn_dev_address_msb, 
+    ttn_net_session_key_msb, 
+    ttn_app_session_key_msb, 
+    ttn_region)
+
+lora = TinyLoRa(spi, cs, irq, rst, ttn_config)
+
+while True:
+    print("Sending packet...")
+    data = bytearray(b"\x48\x65\x6C\x6C\x6F")
+    lora.send_data(data, len(data), lora.frame_counter)
+    lora.frame_counter += 1
+    print("done.")
+    time.sleep(30)
