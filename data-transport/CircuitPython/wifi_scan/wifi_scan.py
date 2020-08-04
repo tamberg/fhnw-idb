90d6c03bbbd4d45c20cccb12aa26ed86ec1f6c67
tamberg <tamberg@yaler.net>
2020-08-04 11:43:56 +0200

    Cleanup.

--- data-transport/CircuitPython/wifi_scan/wifi_scan.py
+++ data-transport/CircuitPython/wifi_scan/wifi_scan.py
@@ -1,11 +1,10 @@
-# Based on 
+# Based on https://github.com/adafruit/Adafruit_CircuitPython_ESP32SPI licensed under MIT License
+# https://circuitpython.readthedocs.io/projects/esp32spi/en/latest/
 
 import board
 import busio
 import digitalio
 from adafruit_esp32spi import adafruit_esp32spi # :)
-
-print("ESP32 SPI hardware test")
 
 # FeatherWing ESP32 AirLift, nRF52840
 cs = digitalio.DigitalInOut(board.D13)
@@ -15,15 +14,10 @@ rst = digitalio.DigitalInOut(board.D12)
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 esp = adafruit_esp32spi.ESP_SPIcontrol(spi, cs, rdy, rst)
 
-if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
-    print("ESP32 found and in idle mode")
-
-print("Firmware version", esp.firmware_version)
-print("MAC addr:", [hex(i) for i in esp.MAC_address])
-
 while True:
-    print("\nScanning...");
-    for ap in esp.scan_networks():
-    	rssi = ap['rssi']
-    	ssid = str(ap['ssid'], 'utf-8')
+    print("\nScanning...")
+    networks = esp.scan_networks()
+    for network in networks:
+    	rssi = network['rssi']
+    	ssid = str(network['ssid'], 'utf-8')
         print("  rssi: %d, ssid: %s" % (rssi, ssid))
