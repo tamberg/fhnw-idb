90c9f2b85ded91cdcf4cb3a102ab96c3c560d33f
tamberg <tamberg@yaler.net>
2020-08-04 10:30:48 +0200

    Cleanup.

--- data-acquisition/CircuitPython/digital_input/digital_input.py
+++ data-acquisition/CircuitPython/digital_input/digital_input.py
@@ -2,10 +2,10 @@ import board
 import digitalio
 import time
 
-button = digitalio.DigitalInOut(board.D9) # nRF52840, Grove D4
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
+sensor = digitalio.DigitalInOut(board.D9) # nRF52840, Grove D4
+sensor.direction = digitalio.Direction.INPUT
+sensor.pull = digitalio.Pull.UP
 
 while True:
-    print(button.value)
+    print(sensor.value)
     time.sleep(0.1)
