10b300b544f51e0190e90cc3c4eb4682ef56cbf1
tamberg <tamberg@yaler.net>
2020-08-04 14:05:40 +0200

    Added example.

---
+++ data-transport/CircuitPython/wifi_addr/wifi_addr.py
@@ -0,0 +1,14 @@
+import board
+import busio
+import digitalio
+from adafruit_esp32spi import adafruit_esp32spi # :)
+
+# FeatherWing ESP32 AirLift, nRF52840
+cs = digitalio.DigitalInOut(board.D13)
+rdy = digitalio.DigitalInOut(board.D11)
+rst = digitalio.DigitalInOut(board.D12)
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, cs, rdy, rst)
+
+print("MAC address:", [hex(i) for i in esp.MAC_address])
