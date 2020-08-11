42c6736411a9ebac9fc744a76b9048449f68b2b4
tamberg <tamberg@yaler.net>
2020-08-11 15:08:46 +0200

    Updated example.

--- data-acquisition/CircuitPython/pwm_buzzer_tones/pwm_buzzer_tones.py
+++ data-acquisition/CircuitPython/pwm_buzzer_tones/pwm_buzzer_tones.py
@@ -2,11 +2,12 @@ import time
 import board
 import pulseio
 
-cycle = 65535 // 2 # on 50%
+cycle = 65535 // 5 # 20% power
 buzzer = pulseio.PWMOut(board.A0,
   duty_cycle=cycle, variable_frequency=True)
 
 while True:
-    for f in (262, 294, 330, 349, 392):
+    for f in (262, 294, 330, 392):
         buzzer.frequency = f
-        time.sleep(0.25)
+        time.sleep(0.1)
+
