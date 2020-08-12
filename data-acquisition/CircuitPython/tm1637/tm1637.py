ad5bf509d98d0af61ec3abda461d9bd8279ebc35
tamberg <tamberg@yaler.net>
2020-08-12 19:12:48 +0200

    Cleanup.

--- data-acquisition/CircuitPython/tm1637/tm1637.py
+++ data-acquisition/CircuitPython/tm1637/tm1637.py
@@ -1,8 +1,8 @@
 import board
 import time
-import tm1637
+import tm1637lib
 
-display = tm1637.Grove4DigitDisplay(board.D9, board.D10) # nRF52840 D9, D10, Grove D4
+display = tm1637lib.Grove4DigitDisplay(board.D9, board.D10) # nRF52840 D9, D10, Grove D4
 
 colon = True
 while True:
