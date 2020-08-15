94c3e2af78b075c8418a964bc98fd48a27b897ff
tamberg <tamberg@yaler.net>
2020-08-15 13:53:25 +0200

    Renamed esp to wifi.

--- data-transport/CircuitPython/wifi_address/wifi_address.py
+++ data-transport/CircuitPython/wifi_address/wifi_address.py
@@ -1,7 +1,7 @@
 import board
 import busio
 import digitalio
-from adafruit_esp32spi import adafruit_esp32spi # :)
+from adafruit_esp32spi import adafruit_esp32spi
 
 # FeatherWing ESP32 AirLift, nRF52840
 cs = digitalio.DigitalInOut(board.D13)
@@ -9,6 +9,6 @@ rdy = digitalio.DigitalInOut(board.D11)
 rst = digitalio.DigitalInOut(board.D12)
 
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-esp = adafruit_esp32spi.ESP_SPIcontrol(spi, cs, rdy, rst)
+wifi = adafruit_esp32spi.ESP_SPIcontrol(spi, cs, rdy, rst)
 
-print("MAC address:", [hex(i) for i in esp.MAC_address])
+print("MAC address:", [hex(i) for i in wifi.MAC_address])
